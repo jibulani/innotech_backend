61c0ab78e1114c36a61aecc03a9c64e5a6154da4
jibulani <evgeni-trofimo@yandex.ru>
2024-02-06 11:00:23 +0300

    add phraseService and tests

--- src/main/java/dev/jibulani/servlet/SupportServlet.java
+++ src/main/java/dev/jibulani/servlet/SupportServlet.java
@@ -1,36 +1,27 @@
 package dev.jibulani.servlet;
 
+import dev.jibulani.service.PhraseService;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ThreadLocalRandom;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SupportServlet extends HttpServlet {
-
-    public static final List<String> cheerUpPhrases = new CopyOnWriteArrayList<>() {{
-        add("У тебя все получится!");
-    }};
+    public PhraseService phraseService = new PhraseService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        resp.getWriter().print(
-                cheerUpPhrases.get(
-                        ThreadLocalRandom.current().nextInt(cheerUpPhrases.size()) % cheerUpPhrases.size()
-                ));
+        resp.getWriter().print(phraseService.getPhrase());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String input = req.getReader().readLine();
-        cheerUpPhrases.add(input);
+        phraseService.addPhrase(input);
         resp.setContentType("text/plain");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         resp.getWriter().print(String.format("Добавлена фраза: %s", input));
--- src/test/java/dev/jibulani/servlet/SupportServletTest.java
+++ src/test/java/dev/jibulani/servlet/SupportServletTest.java
@@ -1,5 +1,6 @@
 package dev.jibulani.servlet;
 
+import dev.jibulani.service.PhraseService;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,34 +13,37 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SupportServletTest {
 
     private static final SupportServlet supportServlet = new SupportServlet();
     private static final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
     private static final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+    private static final PhraseService phraseService = Mockito.mock(PhraseService.class);
     private static StringWriter writer;
 
     @BeforeAll
     public static void setup() throws IOException {
         writer = new StringWriter();
         Mockito.when(response.getWriter()).thenReturn(new PrintWriter(writer));
+        supportServlet.phraseService = phraseService;
     }
 
     @Test
     public void shouldGetPhraseFromList() throws ServletException, IOException {
+        String resultPhrase = "phrase";
+        Mockito.when(phraseService.getPhrase()).thenReturn(resultPhrase);
         supportServlet.doGet(request, response);
-        assertTrue(SupportServlet.cheerUpPhrases.contains(writer.toString()));
+        assertEquals(resultPhrase, writer.toString());
+        Mockito.verify(phraseService).getPhrase();
     }
 
     @Test
     public void shouldPostPhraseToList() throws ServletException, IOException {
         String newPhrase = "new phrase";
-        assertFalse(SupportServlet.cheerUpPhrases.contains(newPhrase));
         Mockito.when(request.getReader()).thenReturn(new BufferedReader(new StringReader(newPhrase)));
         supportServlet.doPost(request, response);
-        assertTrue(SupportServlet.cheerUpPhrases.contains(newPhrase));
+        Mockito.verify(phraseService).addPhrase(newPhrase);
     }
 }
